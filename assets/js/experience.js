8307bbdae34452fa085940914e445858777b7dfc
Deepak Rajput <createcode2gether@gmail.com>
2022-04-17 15:51:13 +0530

    syntax fix

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -20,42 +20,42 @@ const exp = [
   },
   {
     title: "Software Developement Intern",
-    cardImage: "assets/images/experience-page/axtrum.pmg",
+    cardImage: "assets/images/experience-page/axtrum.png",
     place: "Axtrum Solutions",
     time: "(Aug 2016 - Feb, 2019)",
     desp: "<li>Worked on the project Activate Here standards Implementation for Electronic software delivery.</li><li>Worked on a software that would help small ecommerce retailers to fetch product keys directly from vendors like Microsoft,Adobe,Kaspersky etc. .</li>",
   },
 ];
 
-const showCards2 = () => {
-  let output = "";
-  exp.forEach(
-    ({ title, cardImage, place, time, desp }) =>
-      (output += `        
-    <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
-      <div class="card card1">
-        <img src="${cardImage}" class="featured-image"/>
-        <article class="card-body">
-          <header>
-            <div class="title">
-              <h3>${title}</h3>
-            </div>
-            <p class="meta">
-              <span class="pre-heading">${place}</span><br>
-              <span class="author">${time}</span>
-            </p>
-            <ol>
-              ${desp}
-            </ol>
-          </header>
-        </article>
-      </div>
-    </div>
-      `)
-  );
-  experiencecards.innerHTML = output;
-};
-document.addEventListener("DOMContentLoaded", showCards2);
+// const showCards2 = () => {
+//   let output = "";
+//   exp.forEach(
+//     ({ title, cardImage, place, time, desp }) =>
+//       (output += `        
+//     <div class="col gaap" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="100" data-aos-duration="400"> 
+//       <div class="card card1">
+//         <img src="${cardImage}" class="featured-image"/>
+//         <article class="card-body">
+//           <header>
+//             <div class="title">
+//               <h3>${title}</h3>
+//             </div>
+//             <p class="meta">
+//               <span class="pre-heading">${place}</span><br>
+//               <span class="author">${time}</span>
+//             </p>
+//             <ol>
+//               ${desp}
+//             </ol>
+//           </header>
+//         </article>
+//       </div>
+//     </div>
+//       `)
+//   );
+//   experiencecards.innerHTML = output;
+// };
+// document.addEventListener("DOMContentLoaded", showCards2);
 
 //Volunteership Cards
 
